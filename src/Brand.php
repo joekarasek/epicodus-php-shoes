a6931e63b0546a9382ee36080c8f6899b8bf0cb6
Joe Karasek <joseph.karasek@gmail.com>
2016-03-04 09:05:56 -0800

    add basic Brand item with getters and setters

---
+++ src/Brand.php
@@ -0,0 +1,59 @@
+<?php
+    class Brand
+    {
+        private $name;
+        private $id;
+
+        function __construct($name, $id=null)
+        {
+            $this->name = $name;
+            $this->id = $id;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO brands (name) VALUES ('{$this->getName()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_brands = $GLOBALS['DB']->query("SELECT * FROM brands;");
+            $brands = array();
+            foreach ($returned_brands as $brand) {
+                $name = $brand['name'];
+                $id = $brand['id'];
+                $new_brand = new Brand($name, $id);
+                array_push($brands, $new_brand);
+            }
+            return $brands;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->query("DELETE FROM brands;");
+        }
+
+        static function find($search_id)
+        {
+            $found_brand = NULL;
+            $brands = Brand::getAll();
+            foreach ($brands as $brand) {
+                if ($brand->getId() == $search_id) {
+                    $found_brand = $brand;
+                }
+            }
+            return $found_brand;
+        }
+    }
+ ?>
---
+++ tests/BrandTest.php
@@ -0,0 +1,109 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once __DIR__ . '/../src/Brand.php';
+
+    $server = 'mysql:host=localhost:8889;dbname=shoes_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class BrandTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Brand::deleteAll();
+        }
+
+        function test_save()
+        {
+            // Arrange
+            $name = 'Nike';
+            $test_brand = new Brand($name);
+
+            // Act
+            $test_brand->save();
+
+            // Assert
+            $result = Brand::getAll();
+            $this->assertEquals($test_brand, $result[0]);
+        }
+
+        function test_getId()
+        {
+            // Arrange
+            $name = 'Nike';
+            $id = 1;
+            $test_brand = new Brand($name, $id);
+
+            // Act
+            $result = $test_brand->getId();
+
+            // Assert
+            $this->assertEquals(1, $result);
+        }
+
+        function test_getAll()
+        {
+            // Arrange
+            $name = 'Nike';
+            $test_brand = new Brand($name);
+            $test_brand->save();
+
+            $name2 = 'Adidadas';
+            $test_brand2 = new Brand($name2);
+            $test_brand2->save();
+
+            // Act
+            $result = Brand::getAll();
+
+            // Assert
+            $this->assertEquals([$test_brand, $test_brand2], $result);
+
+        }
+
+        function test_deleteAll()
+        {
+            // Arrange
+            $name = 'Nike';
+            $test_brand = new Brand($name);
+            $test_brand->save();
+
+            $name2 = 'Air Jordans';
+            $test_brand2 = new Brand($name2);
+            $test_brand2->save();
+
+            // Act
+            Brand::deleteAll();
+            $result = Brand::getAll();
+
+            // Assert
+            $this->assertEquals([], $result);
+
+        }
+
+        function test_find()
+        {
+            // Arrange
+            $name = 'Nike';
+            $test_brand = new Brand($name);
+            $test_brand->save();
+
+            $name2 = 'Puma';
+            $test_brand2 = new Brand($name2);
+            $test_brand2->save();
+
+            // Act
+            $id = $test_brand->getId();
+            $result = Brand::find($id);
+
+            // Assert
+            $this->assertEquals($test_brand, $result);
+        }
+
+    }
+ ?>
